54081cec952b40d3dc2e073ff5ca1342fe5bf978
Jeff DeSurra <jeffdesurra@macbook-pro.attlocal.net>
2018-04-21 17:39:28 -0700

    Added sample gameboards to minesweeper.js

---
+++ src/minesweeper.js
@@ -0,0 +1,22 @@
+// Create an empty gameboard line
+const blankLine = '  |   |  ';
+
+// log to console a message about the gameboard
+console.log('This is what an empty gameboard would look like:');
+
+// Log the sample gameboard to console
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+// build sample const
+const guessLine = '1 |   |  ';
+const bombLine = '  | B |  ';
+
+// Log message about sample gameboard
+console.log('This is what a sample gameboard would look like:');
+
+// Log the sample gameboard to the console
+console.log(guessLine);
+console.log(blankLine);
+console.log(blankLine);
