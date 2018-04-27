abd602f47c804b56c57681e0146b1181f5b3880a
thejeqff <desjeff@gmail.com>
2018-04-26 21:25:28 -0700

    Updated code to randomly generate player and bomb boards
    This commit is part 3 of the minesweeper project. It randomly generates an empty player board , a bomb board with bombs, and draws them to the screen.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,13 +1,74 @@
-const printBoard = board => {
-  console.log('Current Board:');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
-}
+// Create the function to generate the player generateBombBoard
+// Returns a fully generated player board
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  // create the variable to hold the board
+  let board = [];
+  // Loop through the number of rows needing to be generated based on the
+  // passed parameter
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    // Create a variable to create a row
+    let row = [];
+    // Loop through each column
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      // Push the completed columns to the row
+      row.push(' ');
+    }
+    // Push the completed row with columns to the board
+    board.push(row);
+  }
+  // Return the completed board
+  return board;
+};
 
-let board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
+// Function creates the game board that holds the bomb locations
+// Returns a completed game board with bomb locations
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  // create the variable to hold the board
+  let board = [];
+  // Loop through the number of rows needing to be generated based on the
+  // passed parameter
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    // Create a variable to create a row
+    let row = [];
+    // Loop through each column
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      // Push a null slot to each place where a bomb isn't held
+      row.push(null);
+    }
+    // Push the completed row with columns to the board
+    board.push(row);
+  }
+  // Place randomly generated bombs on the board
+  // Track the number of bombs placed
+  numberOfBombsPlaced = 0;
+  // Continue looping until all bombs have been placed on the board
+  while (numberOfBombsPlaced < numberOfBombs) {
+    // Get a random row number
+    let randomRowIndex = Math.floor(Math.random() * Math.floor(numberOfRows));
+    // Get a random column number
+    let randomColumnIndex = Math.floor(Math.random() * Math.floor(numberOfColumns));
 
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+    // TODO: create conditional logic that prevents placing duplicate bombs
+    // Place the bomb at the selected location and increment the bomb counter
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+  }
+  // Return the completed board
+  return board;
+};
+
+// Create a function to print boards
+const printBoard = (board) => {
+  // Join the columns together using pipes, the join the rows on different lines
+  // and log the board to the console
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+// Test the functions, creating a 3 x 4 player board
+// and a bomb board with 5 bombs and print them
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+console.log("Player board: ");
+printBoard(playerBoard);
+console.log("Bomb board: ");
+printBoard(bombBoard);
