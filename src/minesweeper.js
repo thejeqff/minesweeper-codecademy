c7b976ccfe2b357320e53f2cff6f23bf96c66203
thejeqff <desjeff@gmail.com>
2018-04-28 09:57:44 -0700

    Fixed bomb finding bug and other syntax errors; added test logic

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -49,7 +49,7 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
     let randomColumnIndex = Math.floor(Math.random() * Math.floor(numberOfColumns));
 
     // Check to make sure a bomb does not exist on this spot
-    if board[randomRowIndex][randomColumnIndex] !== 'B') {
+    if (board[randomRowIndex][randomColumnIndex] !== 'B') {
       // Place the bomb at the selected location and increment the bomb counter
       board[randomRowIndex][randomColumnIndex] = 'B';
       numberOfBombsPlaced++;
@@ -63,18 +63,27 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
 const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
   // Constant that identifies all possible offset (adjacent) squares
   // to selected square
-  const neighborOffsets = [[1,1],[1,0],[1,-1],[0,1],[0,-1],[-1,1],[-1,0],[-1,-1]];
+  const neighborOffsets = [[rowIndex + 1,columnIndex + 1],
+                           [rowIndex + 1, columnIndex],
+                           [rowIndex + 1,columnIndex - 1],
+                           [rowIndex, columnIndex + 1],
+                           [rowIndex, columnIndex - 1],
+                           [rowIndex - 1, columnIndex + 1],
+                           [rowIndex - 1, columnIndex],
+                           [rowIndex - 1, columnIndex - 1]];
   // Constants that get the board dimensions
   const numberOfRows = bombBoard.length;
-  const numberOfColumns = bombboard[0].length;
+  const numberOfColumns = bombBoard[0].length;
   // Variable that determines the number of adjacent bombs
   let numberOfBombs = 0;
   // Check each offset to determine the number of bombs next to it
   neighborOffsets.forEach(offset => {
-    const neighborRowIndex = offset[rowIndex];
-    const neighborColumnIndex = offset[columnIndex];
-    if (neighborRowIndex > 0 && neighborRowIndex < numberOfRows &&
-        neighborColumnIndex > 0 && neighborColumnIndex < numberOfColumns) {
+    const neighborRowIndex = offset[0];
+    const neighborColumnIndex = offset[1];
+    // Check to make sure that the array position is valid
+    if (neighborRowIndex >= 0 && neighborRowIndex < numberOfRows &&
+        neighborColumnIndex >= 0 && neighborColumnIndex < numberOfColumns) {
+          // If board position is valid, check for a bomb and increment if found
           if (bombBoard[neighborRowIndex][neighborColumnIndex] === 'B') {
             numberOfBombs++;
           }
@@ -82,6 +91,20 @@ const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
   });
   // Return the number of bombs next to this node
   return numberOfBombs;
+};
+
+//Create function that flips a tile over
+const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
+  // Check to see if this tile has already been flipped
+  if (playerBoard[rowIndex][columnIndex] !== ' ' ) {
+    console.log('This tile has already been flipped!');
+    // If not flipped, check to see if there is a bomb under that tile
+  } else if (bombBoard[rowIndex][columnIndex] === 'B') {
+    playerBoard[rowIndex][columnIndex] = 'B';
+    // If both of those conditions are passed, get the number of neighboring bombs
+  } else {
+    playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard, rowIndex, columnIndex);
+  }
 };
 
 // Create a function to print boards
@@ -99,3 +122,7 @@ console.log("Player board: ");
 printBoard(playerBoard);
 console.log("Bomb board: ");
 printBoard(bombBoard);
+// Testing tile flips
+flipTile(playerBoard, bombBoard, 1, 2);
+console.log('Updated player board:');
+printBoard(playerBoard);
